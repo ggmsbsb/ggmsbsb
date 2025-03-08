e75198fe3fc7cccbabe411b220a2b4dfe0607129
ggmsbsb <guilhermem20g@gmail.com>
2025-03-08 12:44:28 -0300

    teste transparencia

--- blink.py
+++ blink.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw
 
 # Imagem que tu quer fazer o gif de piscar
-logo = Image.open("logo.png")
+logo = Image.open("logo.png").convert("RGBA")
 
 num_frames = 120
 frame_duration = 50  # duração em milissegundos da animação
@@ -14,7 +14,6 @@ for i in range(num_frames):
     frame = Image.new("RGBA", logo.size, (0, 0, 0, 0))
     
     # Animação de piscar. Feito baseado na opacidade.
-    draw = ImageDraw.Draw(frame)
     opacity = int(255 * (1 - abs((i / (num_frames / 2)) - 1)))
     logo_with_opacity = logo.copy()
     logo_with_opacity.putalpha(opacity)
@@ -26,4 +25,4 @@ for i in range(num_frames):
     frames.append(frame)
 
 # Salvar os frames como um GIF
-frames[0].save("logo_blink.gif", save_all=True, append_images=frames[1:], duration=frame_duration, loop=0)
+frames[0].save("logo_blink.gif", save_all=True, append_images=frames[1:], duration=frame_duration, loop=0, transparency=0)
